0be97dc1026a940a18a2346aa7e69a4b506074ff
eric-k-chu <kiyuen88ec@gmail.com>
2024-01-25 01:11:18 -0800

    completed svelte-counter

---
+++ svelte-counter/src/App.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  let count = 0;
+
+  function increment() { 
+    count += 1;
+  }
+
+  function decrement() { 
+    count -= 1;
+  }
+
+</script>
+
+<main>
+  <button on:click={decrement}>Down</button>
+  <h1>{count}</h1>
+  <button on:click={increment}>Up</button>
+</main>
+
+<style>
+  main {
+    display: flex;
+    min-height: 100vh;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+  }
+
+  button {
+    border: none;
+    padding: .5rem 2rem;
+    height: fit-content;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+</style>
