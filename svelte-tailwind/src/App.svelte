aec45a4a829e89b730acda50b14f25644c73725c
eric-k-chu <kiyuen88ec@gmail.com>
2024-01-25 16:15:02 -0800

    completed svelte-tailwind

---
+++ svelte-tailwind/src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  let count = 0;
+
+  function manageCount(op: "+" | "-"): void {
+    if (op === '+') count += 1;
+    else count -=1;
+  }
+</script>
+
+<main class="flex items-center justify-center min-h-screen gap-4">
+  <button on:click={() => manageCount("-")} class="px-4 py-1 text-lg font-medium bg-neutral-700 rounded-md hover:bg-neutral-600">-</button>
+  <h1 class="min-w-16 truncate text-center">{count}</h1>
+  <button on:click={() => manageCount("+")} class="px-4 py-1 text-lg font-medium bg-neutral-700 rounded-md hover:bg-neutral-600">+</button>
+</main>
